2f85b518a9943eedf5b05699b61b15aa149962f2
dui <32657584+phamleduy04@users.noreply.github.com>
2022-07-27 01:33:52 -0500

    update deps and cleanup

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
@@ -15,14 +13,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
 app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
 
 // error handler
 app.use((err, req, res, next) => {
